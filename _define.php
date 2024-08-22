5f9be34eb18ab7f2215a119edd26ee2275fe971b
franck <carnet.franck.paul@gmail.com>
2024-08-22 08:20:22 +0200

    New release 7.3

--- _define.php
+++ _define.php
@@ -14,7 +14,7 @@ $this->registerModule(
     'Spamplemousse2',
     'A bayesian spam filter for dotclear',
     'Alain Vagner and contributors',
-    '7.2',
+    '7.3',
     [
         'requires'    => [['core', '2.28']],
         'type'        => 'plugin',
