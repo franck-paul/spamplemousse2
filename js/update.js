664f2aa0a8f98a1e36228ccbb42798f890ae9920
franck <carnet.franck.paul@gmail.com>
2024-12-25 10:20:17 +0100

    JS Code review (WIP)

--- js/update.js
+++ js/update.js
@@ -1,7 +1,7 @@
 /*global $, dotclear, progressUpdate */
 'use strict';
 
-$(() => {
+dotclear.ready(() => {
   $('#next').hide();
   const data = dotclear.getData('spamplemousse2');
   progressUpdate(data.funcClass, data.funcMethod, data.pos, data.start, data.stop, data.baseInc);
